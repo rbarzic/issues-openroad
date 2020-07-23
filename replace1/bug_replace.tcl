3b301674e1a12b0cb21810ff5d2cd960dd38bcde
Ronan BARZIC <ronan.barzic@onio.com>
2020-07-23 20:03:15 +0200

    Add an example for an issue with global_placement command and pad pins

---
+++ replace1/bug_replace.tcl
@@ -0,0 +1,10 @@
+read_lef NangateOpenCellLibrary.tech.lef
+read_lef NangateOpenCellLibrary.macro.mod.lef
+read_lef bytewrite_ram_32bits_metal.lef
+read_lef iocells_metal.lef
+
+read_def chip.nangate45.pdn.def
+
+auto_place_pins metal1
+global_placement
+
